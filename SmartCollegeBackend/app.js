038ca01a9952aeb431360a0a064240625386582d
Rohidas Lakade <rohidaslakade0707@gmail.com>
2026-01-10 00:46:38 +0530

    Added student feature

--- SmartCollegeBackend/app.js
+++ SmartCollegeBackend/app.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const morgan = require("morgan");
-
-// dotenv.config();
-
-// const connectDB = require("./src/config/db");
-
-// const app = express();
-// connectDB();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("dev"));
-
-// app.use("/api/auth", require("./src/routes/auth.routes"));
-// app.use("/api/departments", require("./src/routes/department.routes"));
-// app.use("/api/courses", require("./src/routes/course.routes"));
-// app.use("/api/students", require("./src/routes/student.routes"));
-// app.use("/api/attendance", require("./src/routes/attendance.routes"));
-
-
-// const PORT = process.env.PORT || 7000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
-
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
